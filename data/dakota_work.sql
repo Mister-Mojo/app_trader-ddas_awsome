e92406322b2aef89f811518dfa0a9f2698dd6a94
DakotaWeedon <dakotaweedon@gmail.com>
2021-10-04 16:05:24 -0500

    Monday's work

--- data/dakota_work.sql
+++ data/dakota_work.sql
@@ -1,51 +1,67 @@
--- Play store
+-- App store list
 
--- Category types
+-- with lifespan as (SELECT DISTINCT(name), (rating * 2)+1 AS expected_lifespan
+-- 				  FROM app_store_apps)
+-- SELECT DISTINCT(name), primary_genre, content_rating, review_count::numeric, expected_lifespan
+-- FROM app_store_apps INNER JOIN lifespan USING (name)
+-- WHERE rating IS NOT NULL AND review_count IS NOT NULL
+-- AND price <= 1
+-- ORDER BY expected_lifespan DESC, review_count DESC
+-- LIMIT 10;
+
+
+
+-- Play store list
+
+-- with lifespan as (SELECT DISTINCT(name), (rating * 2)+1 AS expected_lifespan
+-- 				  FROM play_store_apps)
+
+-- SELECT DISTINCT(name), category, content_rating, review_count, expected_lifespan 
+-- FROM play_store_apps INNER JOIN lifespan USING (name)
+-- WHERE rating > 4.7
+-- AND rating IS NOT NULL AND review_count IS NOT NULL
+-- AND price IN ('0', '$0.99', '$1.00')
+-- AND category IN ('GAME', 'FAMILY')
+-- ORDER BY review_count DESC, expected_lifespan DESC
+-- LIMIT 12;
+
+
+
+-- In both list
+
+-- WITH in_both AS (SELECT DISTINCT(name)
+-- 			 FROM app_store_apps INNER JOIN play_store_apps USING (name))
+			 
+-- SELECT DISTINCT(name), primary_genre, review_count::numeric, (rating * 2)+1 AS expected_lifespan
+-- FROM app_store_apps INNER JOIN in_both USING (name)
+-- WHERE rating IS NOT NULL AND review_count IS NOT NULL
+-- AND price <= 1
+-- AND primary_genre IN ('Games')
+-- ORDER BY expected_lifespan DESC, review_count DESC
+-- LIMIT 10;
+
+
+-- App store category count
+
+-- SELECT primary_genre, COUNT(primary_genre)
+-- FROM app_store_apps
+-- GROUP BY primary_genre
+-- ORDER BY COUNT(primary_genre) DESC;
+
+-- Play store category count
 
 -- SELECT category, COUNT(category)
 -- FROM play_store_apps
 -- GROUP BY category
 -- ORDER BY COUNT(category) DESC;
 
--- Genre types
 
--- SELECT genres, COUNT(genres)
--- FROM play_store_apps
--- GROUP BY genres
--- ORDER BY COUNT(genres) DESC;
 
--- Type of app in terms of payment
 
--- SELECT type, COUNT(type)
--- FROM play_store_apps
--- GROUP BY type
--- ORDER BY COUNT(type);
 
--- Count of app ratings
 
--- SELECT CASE WHEN rating IS NULL THEN 'no_rating'
--- 	   		WHEN rating < 1 THEN '0-0.99'
--- 			WHEN rating BETWEEN 1 AND 1.99 THEN '1-1.99'
--- 			WHEN rating BETWEEN 2 AND 2.99 THEN '2-2.99'
--- 			WHEN rating BETWEEN 3 AND 3.99 THEN '3-3.99'
--- 			WHEN rating BETWEEN 4 AND 4.99 THEN '4-4.99'
--- 			ELSE '5+' END AS rating_group,
--- 		COUNT(CASE WHEN rating IS NULL THEN 'no_rating'
--- 			WHEN rating < 1 THEN '0-0.99'
--- 			WHEN rating BETWEEN 1 AND 1.99 THEN '1-1.99'
--- 			WHEN rating BETWEEN 2 AND 2.99 THEN '2-2.99'
--- 			WHEN rating BETWEEN 3 AND 3.99 THEN '3-3.99'
--- 			WHEN rating BETWEEN 4 AND 4.99 THEN '4-4.99'
--- 			ELSE '5+' END) AS count_rating_group
--- FROM play_store_apps
--- GROUP BY rating_group
--- ORDER BY rating_group DESC;
 
---Apps that share name among both
 
--- SELECT DISTINCT(name)
--- FROM app_store_apps INNER JOIN play_store_apps USING (name)
--- WHERE category ILIKE 'game';
 
 
 
